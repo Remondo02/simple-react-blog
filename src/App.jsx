28638eed78a74bbdaca482801b7d967ddb26a2f2
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-08 18:03:46 +0900

    Navigation base don hash ok & page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useHashNavigation } from "./hooks/useHashNavigation.js"
+import { Home } from "./pages/Home.jsx"
+import { Contact } from "./pages/Contact.jsx"
+import { Post } from "./pages/Post.jsx"
+import { NotFound } from "./pages/NotFound.jsx"
 
 function App() {
   const { page } = useHashNavigation()
+  const pageContent = getPageContent(page)
 
   return (
     <>
@@ -11,8 +16,22 @@ function App() {
         <a href="#post">Article</a>
         <a href="#contact">Contact</a>
       </p>
+      {pageContent}
     </>
   )
+}
+
+function getPageContent(page) {
+  if (page === "home") {
+    return <Home />
+  }
+  if (page === "contact") {
+    return <Contact />
+  }
+  if (page === "post") {
+    return <Post />
+  }
+  return <NotFound page={page} />
 }
 
 export default App
